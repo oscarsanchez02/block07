9c4b4509b92109f68bed4aebfdc7b762cfa7d723
oscar sanchez <oscarsanchez02@yahoo.com>
2024-02-01 10:51:09 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Create three variables to store the results of the combination.
+var str1, str2, str3;
+
+// Use three different arithmetic operators to generate the three codes.
+str1 = 5 + 5; 
+str2 = 4 * 10;
+str3 = 41 - 2; 
+
+// Must create the combination variable that will equal all the veriables in math format to get the code.
+var combination = str1 + '-' + str2 + '-' + str3;
+
+// Dialog box will promted with an alert pop-up that will have text's followed by adding the "combination" variable.
+alert('You have received this message because you have been chosen to open an important vault. Here is the secret combination: ' + combination);
